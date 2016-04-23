5bb12a27c05d00da918972a9dc39db2d64134f75
Leah <leahpwatt@gmail.com>
2016-04-23 18:05:06 -0400

    added stylus, made responsive

--- script.js
+++ script.js
@@ -1,87 +1,10 @@
-// // Lab 6 Results
-
-// // Conversion 1:
-
-function convertTemp (temp, unit) {
-   if (unit === "F") {
- 	console.log ((temp - 32) * 5/9);
- }
-   else {
- 	console.log ((temp * 9/5) + 32);
- }
-}
-
-
-var temp = 212;
-var unit = "F";
-convertTemp(temp, unit);
-
-
-// // Conversion 2:
-
-
-function convertTemp (temp, unit) {
-   if (unit === "F") {
- 	console.log ((temp - 32) * 5/9);
- }
-   else {
- 	console.log ((temp * 9/5) + 32);
- }
-}
-
-
-var temp = 32;
-var unit = "F";
-convertTemp(temp, unit);
-
-
-// // Conversion 3:
-
-
-function convertTemp (temp, unit) {
-   if (unit === "F") {
- 	console.log ((temp - 32) * 5/9);
- }
-   else {
- 	console.log ((temp * 9/5) + 32);
- }
-}
-
-
-var temp = 65;
-var unit = "C";
-convertTemp(temp, unit);
-
-
-// // Conversion 4:
-
-
-function convertTemp (temp, unit) {
-   if (unit === "F") {
- 	console.log ((temp - 32) * 5/9);
- }
-   else {
- 	console.log ((temp * 9/5) + 32);
- }
-}
-
-
-var temp = -40;
-var unit = "F";
-convertTemp(temp, unit);
-
-
-// Bonus
-
-function convertTemp () {
-	var temp=prompt("Please enter a temperature.");
-    var unit=prompt("Please enter C or F");
+button.addEventListener("click", function(){
+	var temp=document.getElementById("temperature").value;
+	var unit=document.getElementById("unit").value;
 	if (unit === "F") {
- 	alert ((temp - 32) * 5/9);
- }
-   else {
- 	alert ((temp * 9/5) + 32);
- }
-}
-
-convertTemp();
+	 	document.getElementById("result").innerHTML = ((temp - 32) * 5/9 + "&#8451");
+	 }
+	   else {
+	 	document.getElementById("result").innerHTML = ((temp * 9/5) + 32 + "&#8457");
+	 }
+	})
